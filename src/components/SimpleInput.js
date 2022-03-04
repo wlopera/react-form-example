2c3b841d5b4623431434051a8c0361d448dc6940
lopera.william@gmail.com <William>
2022-03-04 12:30:16 -0500

    un formulario con unaa sola entrada - validaciones

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -2,41 +2,31 @@ import { useState } from "react";
 
 const SimpleInput = (props) => {
   const [enteredName, setEnteredName] = useState("");
-  const [enteredNamesIsValid, setEnteredNamesIsValid] = useState(false);
   const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+  const enteredNamesIsValid = enteredName.trim() !== "";
+  const nameInputIsInValid = !enteredNamesIsValid && enteredNameTouched;
 
   const nameInputChangeHandler = (event) => {
     setEnteredName(event.target.value);
-
-    if (event.target.value.trim() !== "") {
-      setEnteredNamesIsValid(true);
-      return;
-    }
   };
 
   const nameInputBlurHandler = (event) => {
     setEnteredNameTouched(true);
-    if (event.target.value === "") {
-      setEnteredNamesIsValid(false);
-      return;
-    }
   };
 
   const formSubmissionHandler = (event) => {
     event.preventDefault();
-
     setEnteredNameTouched(true);
 
-    if (enteredName.trim() === "") {
-      setEnteredNamesIsValid(false);
+    if (!enteredNamesIsValid) {
       return;
     }
 
+    console.log("Nombre:", enteredName);
+    setEnteredNameTouched(false);
     setEnteredName("");
-    setEnteredNamesIsValid(true);
   };
-
-  const nameInputIsInValid = !enteredNamesIsValid && enteredNameTouched;
 
   const nameInputClasses = nameInputIsInValid
     ? "form-control invalid"
